d85c82e069b9e0ed4c27110ebf7d2172c8128d60
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2015-12-13 20:16:42 +0800

    Refactor some code

--- FTWCache/FTWCache.swift
+++ FTWCache/FTWCache.swift
@@ -61,24 +61,3 @@ class FTWCache: NSObject {
         data.writeToFile(fileName, atomically: true)
     }
 }
-
-extension String {
-    func md5Hash() -> String! {
-        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
-        let strLen = CUnsignedInt(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
-        
-        CC_MD5(str!, strLen, result)
-        
-        let hash = NSMutableString()
-        for i in 0..<digestLen {
-            hash.appendFormat("%02x", result[i])
-        }
-        
-        result.destroy()
-        
-        return String(format: hash as String)
-    }
-}
-
--- FTWCacheDemo/ViewController.swift
+++ FTWCacheDemo/ViewController.swift
@@ -54,3 +54,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 }
 
+extension String {
+    func md5Hash() -> String! {
+        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let strLen = CUnsignedInt(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
+        
+        CC_MD5(str!, strLen, result)
+        
+        let hash = NSMutableString()
+        for i in 0..<digestLen {
+            hash.appendFormat("%02x", result[i])
+        }
+        
+        result.destroy()
+        
+        return String(format: hash as String)
+    }
+}
+
